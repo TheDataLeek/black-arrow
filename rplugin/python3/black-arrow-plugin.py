c1853df482fc53a0710bd2a986b9ade68f93d88c
Zoë Farmer <zoe@dataleek.io>
2016-07-15 22:22:38 -0700

    shdf

--- rplugin/python3/black-arrow-plugin.py
+++ rplugin/python3/black-arrow-plugin.py
@@ -7,7 +7,7 @@ class BlackArrow(object):
     def __init__(self, vim):
         self.vim = vim
 
-    @neovim.autocmd('BlackArrow')
+    @neovim.command('BlackArrow')
     def search(self):
         self.vim.command('echo "hello"')
         return 10
