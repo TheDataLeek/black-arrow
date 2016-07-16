0aba6c728d35873fb94405ac514049f60ee31f89
Zoë Farmer <zoe@dataleek.io>
2016-07-15 22:01:34 -0700

    Restructure

--- rplugin/python3/black-arrow-plugin.py
+++ rplugin/python3/black-arrow-plugin.py
@@ -1,13 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.5
 
 import neovim
+
+print('here')
 
 @neovim.plugin
 class BlackArrow(object):
     def __init__(self, vim):
+        print('test')
         self.vim = vim
 
-    @neovim.function('search')
+    @neovim.function('BlackArrow', sync=True)
     def search(self, args):
         self.vim.command('echo "hello"')
+        return 5
 
