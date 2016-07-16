c436ce1a06bcbb84e1eef5eaa710003a0955d48f
Zoë Farmer <zoe@dataleek.io>
2016-07-16 00:39:15 -0700

    Testing video

--- rplugin/python3/blackarrow/__init__.py
+++ rplugin/python3/blackarrow/__init__.py
@@ -24,4 +24,4 @@ class BlackArrow(object):
         # blackarrow.start_search(args)
         # self.vim.out_write(sys.stdout.getvalue())
 
-        self.vim.out_write('tsskjhasdfkljhasdfkhaskdfhaslkjfhaslkjdfh')
+        self.vim.current.line = 'tsskjhasdfkljhasdfkhaskdfhaslkjfhaslkjdfh'
