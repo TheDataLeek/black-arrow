71aff46bfb2aed5585c5f645cd127f3490dc4a9e
Zoë Farmer <zoe@dataleek.io>
2016-07-15 22:25:36 -0700

    test

--- rplugin/python3/black-arrow-plugin.py
+++ rplugin/python3/black-arrow-plugin.py
@@ -11,3 +11,29 @@ class BlackArrow(object):
     def search(self):
         self.vim.command('echo "hello"')
         return 10
+
+    @neovim.command('Cmd', range='', nargs='*', sync=True)
+    def command_handler(self, args, range):
+        self._increment_calls()
+        self.vim.current.line = (
+            'Command: Called %d times, args: %s, range: %s' % (self.calls,
+                                                               args,
+                                                               range))
+
+    @neovim.autocmd('BufEnter', pattern='*.py', eval='expand("<afile>")',
+                    sync=True)
+    def autocmd_handler(self, filename):
+        self._increment_calls()
+        self.vim.current.line = (
+            'Autocmd: Called %s times, file: %s' % (self.calls, filename))
+
+    @neovim.function('Func')
+    def function_handler(self, args):
+        self._increment_calls()
+        self.vim.current.line = (
+            'Function: Called %d times, args: %s' % (self.calls, args))
+
+    def _increment_calls(self):
+        if self.calls == 5:
+            raise Exception('Too many calls!')
+        self.calls += 1
