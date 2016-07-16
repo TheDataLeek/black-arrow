98052c56afcbde959d6c3bbb8bc8cefe5c8aad24
Zoë Farmer <zoe@dataleek.io>
2016-07-15 22:56:17 -0700

    update

---
+++ rplugin/python3/black_arrow_plugin.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.5
+
+import neovim
+import blackarrow
+
+@neovim.plugin
+class BlackArrow(object):
+    def __init__(self, vim):
+        self.vim = vim
+
+    @neovim.command('BlackArrow', nargs=1)
+    def search(self, args):
+        self.vim.command('echo "{}"'.format(args))
+
+
---
+++ rplugin/python3/blackarrow.py
@@ -0,0 +1,1 @@
+../../black-arrow/blackarrow.py
