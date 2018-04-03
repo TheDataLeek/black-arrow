8dbd3e3ebd4885594b9bb677adf78e2efa5c3bb2
Zoe Farmer <zfarmer@talusanalytics.com>
2018-04-02 21:48:26 -0600

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as readmefobj:
 
 setup(
     name='blackarrow',
-    version='1.0.1',
+    version='1.0.2',
     description='A fast keyword searcher',
     long_desc=long_desc,
     long_description_content_type='text/markdown',
