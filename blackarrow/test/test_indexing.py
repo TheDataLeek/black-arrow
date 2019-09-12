2607151c00902e484a6cabcc4aee9ef8d4212f8c
Zoë Farmer <zoe@dataleek.io>
2019-09-12 09:59:46 -0600

    Fixed failing tests

--- blackarrow/test/test_indexing.py
+++ blackarrow/test/test_indexing.py
@@ -1,18 +1,31 @@
 from ..blackarrow import index_worker
 
+import re
 import pytest
 import queue
 
 
 @pytest.fixture
 def results():
+    """
+    def index_worker(
+        directories: List[str],
+        ignore_re: RETYPE,
+        filename_re: RETYPE,
+        workers: int,
+        search_queue: mp.Queue,
+        depth: int,
+        block=False,
+    ) -> None:
+    :return:
+    """
     input_queue = queue.Queue()
     index_worker(
         ['sample'],  # directories to look in
-        None,  # things to ignore
+        re.compile("a^"), # things to ignore
+        re.compile('test'),  # things to search for
         1,  # num workers
         input_queue,  # where to put results
-        None,   # output (not needed)
         None,   # depth (not needed)
         block=True   # whether or not to block
     )
